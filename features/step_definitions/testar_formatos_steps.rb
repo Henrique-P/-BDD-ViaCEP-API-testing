8983b1c27d36f08d5b7df89f35a3ae3c1ce70d6f
Pedro H <pedrosantana762@gmail.com>
2021-07-28 17:44:47 -0300

    changed variable names

--- features/step_definitions/testar_formatos_steps.rb
+++ features/step_definitions/testar_formatos_steps.rb
@@ -1,33 +1,33 @@
 Quando('envio um GET com um cep válido e solicito a resposta em dado {string}') do |formato|
-  @piped_format = /.\|/
-  @querty_format = /.&/
-  @json_format = /\{\s./
-  @xml_format = /xml/
+  formato_piped = /.\|/
+  formato_querty = /.&/
+  formato_json = /(?:\{\s)/ # /\{\s./
+  formato_xml = /xml/
   case formato
   when 'json'
     @received = Viacep.get('/11712510/json')
-    expect(@received.body.match?(@json_format)).to be_truthy
-    expect(@received.body.match?(@xml_format)).to be_falsey
-    expect(@received.body.match?(@querty_format)).to be_falsey
-    expect(@received.body.match?(@piped_format)).to be_falsey
+    expect(@received.body.match?(formato_json)).to be_truthy
+    expect(@received.body.match?(formato_xml)).to be_falsey
+    expect(@received.body.match?(formato_querty)).to be_falsey
+    expect(@received.body.match?(formato_piped)).to be_falsey
   when 'xml'
     @received = Viacep.get('/11712510/xml')
-    expect(@received.body.match?(@xml_format)).to be_truthy
-    expect(@received.body.match?(@querty_format)).to be_falsey
-    expect(@received.body.match?(@piped_format)).to be_falsey
-    expect(@received.body.match?(@json_format)).to be_falsey
+    expect(@received.body.match?(formato_xml)).to be_truthy
+    expect(@received.body.match?(formato_querty)).to be_falsey
+    expect(@received.body.match?(formato_piped)).to be_falsey
+    expect(@received.body.match?(formato_json)).to be_falsey
   when 'querty'
     @received = Viacep.get('/11712510/querty')
-    expect(@received.body.match?(@querty_format)).to be_truthy
-    expect(@received.body.match?(@xml_format)).to be_falsey
-    expect(@received.body.match?(@piped_format)).to be_falsey
-    expect(@received.body.match?(@json_format)).to be_falsey
+    expect(@received.body.match?(formato_querty)).to be_truthy
+    expect(@received.body.match?(formato_xml)).to be_falsey
+    expect(@received.body.match?(formato_piped)).to be_falsey
+    expect(@received.body.match?(formato_json)).to be_falsey
   else
     @received = Viacep.get('/11712510/piped')
-    expect(@received.body.match?(@piped_format)).to be_truthy
-    expect(@received.body.match?(@json_format)).to be_falsey
-    expect(@received.body.match?(@querty_format)).to be_falsey
-    expect(@received.body.match?(@xml_format)).to be_falsey
+    expect(@received.body.match?(formato_piped)).to be_truthy
+    expect(@received.body.match?(formato_json)).to be_falsey
+    expect(@received.body.match?(formato_querty)).to be_falsey
+    expect(@received.body.match?(formato_xml)).to be_falsey
   end
 end
 
